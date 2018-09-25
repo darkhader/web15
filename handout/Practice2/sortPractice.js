bb25141bc0e956c7a08571643782db32c071f4d2
Nguyễn Hoàng Hiệp <40733446+darkhader@users.noreply.github.com>
2018-09-25 23:38:32 +0700

    Update sortPractice.js

--- handout/Practice2/sortPractice.js
+++ handout/Practice2/sortPractice.js
@@ -1,7 +1,13 @@
-'use strict'
-
 function sort(input) {
-  return input.sort((a,b) => a-b); // Remove this line and change to your own algorithm
+    var temp = input[0];
+    for (let i = 0; i < input.length - 1; i++) {
+        for (let j = i + 1; j < input.length; j++) {
+            if (input[i] > input[j]) {
+                temp = input[j];
+                input[j] = input[i];
+                input[i] = temp;
+            }
+        }
+    }
+    return input;
 }
-
-module.exports = sort
