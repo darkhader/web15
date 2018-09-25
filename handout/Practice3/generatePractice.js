ed74940f132dcd73c1edae6207ca3247b4e7454c
Nguyễn Hoàng Hiệp <40733446+darkhader@users.noreply.github.com>
2018-09-25 23:38:56 +0700

    Update generatePractice.js

--- handout/Practice3/generatePractice.js
+++ handout/Practice3/generatePractice.js
@@ -1,13 +1,46 @@
-'use strict'
 
-function generate(testLengthArray){
-  return Array.from({length : testLengthArray.length})
-    .map(item => ({
-      input: Array.from({length: item}).map(item => []),
-      target: 0,
-      output: -1
-    })
-  ); // Remove this line and change to your own algorithm
+function generate(input, target) {
+    const map = [];
+    var output = -1;
+    var temp = input[0];
+    for (let i = 0; i < input.length; i++) {
+
+        if (input[i] == target)
+            output = i + 1;
+    }
+    for (let i = 0; i < input.length - 1; i++) {
+        for (let j = i + 1; j < input.length; j++) {
+            if (input[i] > input[j]) {
+                temp = input[j];
+                input[j] = input[i];
+                input[i] = temp;
+            }
+        }
+    }
+    for (let i = 0; i < input.length; i++) {
+        map[i] = input[i];
+    }
+ if (map.length >= 4) {
+        if (output == -1) {
+            console.log("input doesn't contain target")
+        }
+        if (output == 1) {
+            console.log("target is at index 0")
+        }
+        if (output == map.length-1) {
+            console.log("target is at index input.length-1")
+        }
+        else if (output != 1 && output!= map.length-1) {
+            console.log("target` is NOT at index `0` or `input.length-1")
+        }
+    }
+
+
+    return {
+        input: map,
+        target: target,
+        output: output
+    }
+   
 }
 
-module.exports = generate
